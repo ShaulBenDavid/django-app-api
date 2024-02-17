1f32121907409851c4db45c5637510b4020555fc
Shaul Ben david <Shaulbd9@gmail.com>
2024-02-17 14:43:36 +0200

    adding database config

---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,42 @@
+"""
+Test custom Django management commands.
+"""
+from unittest.mock import patch
+
+from psycopg2 import OperationalError as Psycopg2OpError
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import SimpleTestCase
+
+
+@patch('core.management.commands.wait_for_db.Command.check')
+class CommandTests(SimpleTestCase):
+    """Test commands."""
+
+    # django.test.testcases.DatabaseOperationForbidden: Database queries
+    # to 'default' are not allowed in SimpleTestCase subclasses
+    # add 'default' to core.tests.test_commands.CommandTest.databases to
+    # silence this failure.
+    # we are using the database in the command to wait_for_db to check
+    # that it is connecte, this is why we have to silence it here
+    databases = ['default']
+
+    def test_wait_for_db_ready(self, patched_check):
+        """Test waiting for database if database ready."""
+        patched_check.return_value = True
+
+        call_command('wait_for_db')
+
+        patched_check.assert_called_once_with(databases=['default'])
+
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        """Test waiting for database when getting OperationalError."""
+        patched_check.side_effect = [Psycopg2OpError] * 2 + \
+            [OperationalError] * 3 + [True]
+
+        call_command('wait_for_db')
+
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.assert_called_with(databases=['default'])
