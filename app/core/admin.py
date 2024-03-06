7db47750286f6f2e0140f41432f4003383d30084
Shaul Ben david <Shaulbd9@gmail.com>
2024-03-06 19:30:51 +0200

    adding admin panel

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,8 @@
-"""
-Django admin customization.
-"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from core.models import User
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'registration_method')
+
+admin.site.register(User, CustomUserAdmin)
