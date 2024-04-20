124a356bf22242f18f6e4cb5370056a6e8131e3a
Shaul Ben david <Shaulbd9@gmail.com>
2024-04-20 15:48:04 +0300

    feat(pagination): fixing pagination to return numbers and not links

--- app/core/utils/pagination.py
+++ app/core/utils/pagination.py
@@ -1,3 +1,4 @@
+from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
 
 
@@ -5,3 +6,20 @@ class StandardResultsSetPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = "page_size"
     max_page_size = 100
+
+    def get_paginated_response(self, data):
+        next_page_number = None
+        previous_page_number = None
+
+        if self.page.has_next():
+            next_page_number = self.page.next_page_number()
+
+        if self.page.has_previous():
+            previous_page_number = self.page.previous_page_number()
+
+        return Response({
+            'next': next_page_number,
+            'previous': previous_page_number,
+            'count': self.page.paginator.count,
+            'results': data
+        })
