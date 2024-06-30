ff0a952e057644cd1b1ed1b63a09153f5ef7e84e
Shaul Ben david <Shaulbd9@gmail.com>
2024-06-30 20:35:00 +0300

    fix

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/zsh
 
 set -e
 
@@ -6,4 +6,4 @@ python manage.py wait_for_db
 python manage.py collectstatic --noinput
 python manage.py migrate
 
-uwasgi --socket :9000 --workers 4 --master --enable-threads --module app.wasgi
+uwasgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
