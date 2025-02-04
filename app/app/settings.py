e42c06b86345fbade70845b4779fcdce1fd452b4
Shaul Ben david <Shaulbd9@gmail.com>
2025-02-04 20:05:21 +0200

    refactor: extend refresh token life time

--- app/app/settings.py
+++ app/app/settings.py
@@ -159,7 +159,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": False,
