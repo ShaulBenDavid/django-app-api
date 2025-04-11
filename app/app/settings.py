35c0159e359d9420987159b4004f67c3c8e7ad47
Shaul Ben david <Shaulbd9@gmail.com>
2025-04-11 18:10:17 +0300

    feat: deploy render

--- app/app/settings.py
+++ app/app/settings.py
@@ -145,9 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/static/"
-MEDIA_URL = "/static/media/"
 
-MEDIA_ROOT = "/vol/web/media"
 STATIC_ROOT = "/vol/web/static"
 
 if not DEBUG:
