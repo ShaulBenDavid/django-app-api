5f25bcafab25dd51511c021047f1f756db375539
Shaul Ben david <Shaulbd9@gmail.com>
2025-04-11 21:15:18 +0300

    feat: deploy render

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,7 +6,7 @@ set -o errexit
 pip install -r requirements.txt
 
 # Convert static asset files
-python manage.py collectstatic --no-input
+python app/manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python manage.py migrate
+python app/manage.py migrate
